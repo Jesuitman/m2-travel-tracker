9a073c961c050d869a447de55c6bf22dfd60f3ec
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-11-10 12:23:14 -0500

    set up variable for testing

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,78 +1,122 @@
-export const getTravellers = () => {
-    return fetch("http://localhost:3001/api/v1/travelers")
-  .then((response) => {
-    if (response.ok) {
-      return response.json(); 
-    } else {
-      throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
-    }
-  })
-  .then((data) => {
-    const travelers = data.travelers; 
-    console.log(travelers);
-  })
-  .catch((error) => {
-    console.error(error);
-  })
-}
+// export const getTravellers = () => {
+//     return fetch("http://localhost:3001/api/v1/travelers")
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json(); 
+//     } else {
+//       throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
+//     }
+//   })
+//   .then((data) => {
+//     const travelers = data.travelers; 
+//     console.log(travelers);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   })
+// }
 
-export const getSingleTraveler = (travelerId) => {
-  return fetch(`http://localhost:3001/api/v1/travelers/${travelerId}`)
-.then((response) => {
-  if (response.ok) {
-    return response.json(); 
-  } else {
-    throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
-  }
-})
-.then((data) => {
-  console.log(data)
-  let traveler
-  if (traveler){
-    traveler = data
-    console.log(traveler) 
-  } else {
-    console.error(`Traveler with ID ${travelerId} not found`)
-  }
-  console.log(traveler);
-})
-.catch((error) => {
-  console.error(error);
-})
-}
+// export const getSingleTraveler = (travelerId) => {
+//   return fetch(`http://localhost:3001/api/v1/travelers/${travelerId}`)
+// .then((response) => {
+//   if (response.ok) {
+//     return response.json(); 
+//   } else {
+//     throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
+//   }
+// })
+// .then((data) => {
+//   console.log(data)
+//   let traveler
+//   if (traveler){
+//     traveler = data
+//     console.log(traveler) 
+//   } else {
+//     console.error(`Traveler with ID ${travelerId} not found`)
+//   }
+//   console.log(traveler);
+// })
+// .catch((error) => {
+//   console.error(error);
+// })
+// }
 
-export const getAllTrips = () => {
-  return fetch("http://localhost:3001/api/v1/trips")
-.then((response) => {
-  if (response.ok) {
-    return response.json(); 
-  } else {
-    throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
-  }
-})
-.then((data) => {
-  const trips = data.trips; 
-  console.log(trips);
-})
-.catch((error) => {
-  console.error(error);
-})
-}
+// export const getAllTrips = () => {
+//   return fetch("http://localhost:3001/api/v1/trips")
+// .then((response) => {
+//   if (response.ok) {
+//     return response.json(); 
+//   } else {
+//     throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
+//   }
+// })
+// .then((data) => {
+//   const trips = data.trips; 
+//   console.log(trips);
+// })
+// .catch((error) => {
+//   console.error(error);
+// })
+// }
 
-export const getAllDestinations = () => {
-  return fetch("http://localhost:3001/api/v1/destinations")
-.then((response) => {
-  if (response.ok) {
-    return response.json(); 
-  } else {
-    throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
-  }
-})
-.then((data) => {
-  const destinations = data.destinations; 
-  console.log(destinations);
-})
-.catch((error) => {
-  console.error(error);
-})
-}
+// export const getAllDestinations = () => {
+//   return fetch("http://localhost:3001/api/v1/destinations")
+// .then((response) => {
+//   if (response.ok) {
+//     return response.json(); 
+//   } else {
+//     throw new Error(`Failed to retrieve data. Status code: ${response.status}`);
+//   }
+// })
+// .then((data) => {
+//   const destinations = data.destinations; 
+//   console.log(destinations);
+// })
+// .catch((error) => {
+//   console.error(error);
+// })
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     loginForm.addEventListener("submit", function (event) {
+//       event.preventDefault();
+  
+//       const username = document.getElementById("username").value;
+//       const password = document.getElementById("password").value;
+  
+//       const traveler = /^travel\d+$/;
+//       if (password === "travel" && traveler.test(username)) {
+//         const travelerId = username.replace("travel", "");
+  
+//         getSingleTraveler(travelerId)
+//           .then((travelerData) => {
+//             if (travelerData) {
+//               welcomeMessage.textContent = `Welcome, ${travelerData.name}!`;
+//               console.log(travelerData);
+//             } else {
+//               alert("Traveler not found. Please try again.");
+//             }
+//           })
+//           .catch((error) => {
+//             console.error(error);
+//           });
+  
+//         loginForm.style.display = "none";
+//       } else {
+//         alert("Invalid login credentials. Please try again.");
+//       }
+//     });
+// });
+// console.log('This is the JavaScript entry file - your code begins here.');
+// getTravellers()
+
+// window.addEventListener("load", () =>{
+//   Promise.all([getTravellers(),()=>getSingleTraveler(travelerId),getAllTrips(),getAllDestinations()])
+//   .then((data) =>{
+//       travelers = data[0]
+//       singleTravler = data[1]()
+//       trips = data[2]
+//       destinations = data[3]
+//   })
+// })
--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-import { getTravellers,getSingleTraveler,getAllTrips,getAllDestinations } from './api-calls';
+// import { getTravellers,getSingleTraveler,getAllTrips,getAllDestinations } from './api-calls';
 
 // query Selectors
 const loginForm = document.getElementById("loginForm");
@@ -13,51 +13,76 @@ import './css/styles.css';
 import './images/turing-logo.png'
 
 let travelerId = 1
-let travelers = []
-let singleTravler = {}
-let trips = []
-let destinations = []
+let travelers = [
+    {
+    id: 1,
+    name: "Ham Leadbeater",
+    travelerType: "relaxer"
+    },
+    {
+    id: 2,
+    name: "Rachael Vaughten",
+    travelerType: "thrill-seeker"
+    }]
+let singleTravler =  {
+    id: 1,
+    name: "Ham Leadbeater",
+    travelerType: "relaxer"
+    }
+let trips = [{
+    id: 89,
+    userID: 2,
+    destinationID: 1,
+    travelers: 5,
+    date: "2019/09/27",
+    duration: 13,
+    status: "approved",
+    suggestedActivities: []
+    },
+    {
+    id: 100,
+    userID: 2,
+    destinationID: 2,
+    travelers: 6,
+    date: "2020/3/28",
+    duration: 10,
+    status: "approved",
+    suggestedActivities: []
+    },
+    {
+    id: 116,
+    userID: 2,
+    destinationID: 3,
+    travelers: 3,
+    date: "2020/04/03",
+    duration: 8,
+    status: "approved",
+    suggestedActivities: []
+    }]
+let destinations = [{
+    id: 1,
+    destination: "Lima, Peru",
+    estimatedLodgingCostPerDay: 70,
+    estimatedFlightCostPerPerson: 400,
+    image: "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    alt: "overview of city buildings with a clear sky"
+    },
+    {
+    id: 2,
+    destination: "Stockholm, Sweden",
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 780,
+    image: "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "city with boats on the water during the day time"
+    },
+    {
+    id: 3,
+    destination: "Sydney, Austrailia",
+    estimatedLodgingCostPerDay: 130,
+    estimatedFlightCostPerPerson: 950,
+    image: "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "opera house and city buildings on the water with boats"
+    }]
 
-window.addEventListener("load", () =>{
-    Promise.all([getTravellers(),()=>getSingleTraveler(travelerId),getAllTrips(),getAllDestinations()])
-    .then((data) =>{
-        travelers = data[0]
-        singleTravler = data[1]()
-        trips = data[2]
-        destinations = data[3]
-    })
-})
 
-document.addEventListener("DOMContentLoaded", function () {
 
-    loginForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-  
-      const traveler = /^travel\d+$/;
-      if (password === "travel" && traveler.test(username)) {
-        const travelerId = username.replace("travel", "");
-  
-        getSingleTraveler(travelerId)
-          .then((travelerData) => {
-            if (travelerData) {
-              welcomeMessage.textContent = `Welcome, ${travelerData.name}!`;
-              console.log(travelerData);
-            } else {
-              alert("Traveler not found. Please try again.");
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-  
-        loginForm.style.display = "none";
-      } else {
-        alert("Invalid login credentials. Please try again.");
-      }
-    });
-});
-// console.log('This is the JavaScript entry file - your code begins here.');
-// getTravellers()

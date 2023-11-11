834d664ad81b1df18bbdbf835822cae9224f5358
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-11-10 20:31:22 -0500

    baseline functionality, login page next

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,18 @@ const approvedTripsSection = document.querySelector('.approved-dashboard-section
 const pastTripsSection = document.querySelector('.past-dashboard-section');
 const costsTripsSection = document.querySelector('.costs-dashboard-section');
 const dashboardTitle = document.querySelector('.dashboard-title')
+const destinationSelect = document.getElementById("destination");
+const newTripButton = document.querySelector("#scheduleTripButton");
+const tripForm = document.querySelector("#newTripForm");
+const dashboard = document.querySelector(".dashboard")
+
+function showElement(element) {
+    element.classList.remove("hidden");
+}
+
+function hideElement(element) {
+    element.classList.add("hidden");
+}
 
 
 // An example of how you tell webpack to use a CSS (SCSS) file
@@ -18,7 +30,7 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-let travelerId = 2
+let userId = 1
 let travelers = [
     {
     id: 1,
@@ -29,12 +41,14 @@ let travelers = [
     id: 2,
     name: "Rachael Vaughten",
     travelerType: "thrill-seeker"
-    }]
+}]
+
 let singleTravler ={
     id: 2,
     name: "Rachael Vaughten",
     travelerType: "thrill-seeker"
-    }
+}
+
 let trips = [{
     id: 89,
     userID: 2,
@@ -74,7 +88,8 @@ let trips = [{
     duration: 13,
     status: "past",
     suggestedActivities: []
-    }]
+}]
+
 let destinations = [{
     id: 1,
     destination: "Lima, Peru",
@@ -98,11 +113,11 @@ let destinations = [{
     estimatedFlightCostPerPerson: 950,
     image: "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     alt: "opera house and city buildings on the water with boats"
-    }]
+}]
 
 
 function displayTrips(userId,status) {
-    const tripsSection = document.querySelector(`.${status}-dashboard-section`);
+    const tripsSection = document.querySelector(`.${status}-inner-dashboard-section`);
     
     const filteredTrips = trips.filter(trip =>trip.userID === userId && trip.status === status);
     
@@ -112,8 +127,8 @@ function displayTrips(userId,status) {
         const tripList = document.createElement('ul');
         tripList.classList.add('trip-list');
         
-        filteredTrips.forEach(trip => {
-          const cost = calculateCostOfTrip(trip)
+    filteredTrips.forEach(trip => {
+        const cost = calculateCostOfTrip(trip)
         const tripItem = document.createElement('li');
         tripItem.innerHTML = `
         <h3>Trip to ${destinations.find(dest => dest.id === trip.destinationID).destination}</h3>
@@ -129,10 +144,10 @@ function displayTrips(userId,status) {
     tripsSection.innerHTML = ""
     tripsSection.appendChild(tripList);
     }
-  }
+}
 
-  document.getElementById("userId").addEventListener("input", function(){
-    const userId = parseInt(this.value)
+document.getElementById("userId").addEventListener("input", function(){
+    userId = parseInt(this.value)
     const userName = getUserNameById(userId)
 
     dashboardTitle.textContent = `Hello ${userName}`
@@ -153,27 +168,69 @@ function calculateCostOfTrip(trip) {
     const costOfTrip = (travelers * estimatedFlightCostPerPerson) + (travelers * estimatedLodgingCostPerDay * duration) * 1.1;
     
     return costOfTrip;
-  }
+}
 
-  function calculateTotalCostForUser(userId) {
+function calculateTotalCostForUser(userId) {
     const userTrips = trips.filter(trip => trip.userID === userId);
     let totalCost = 0;
   
     userTrips.forEach(trip => {
-      const costOfTrip = calculateCostOfTrip(trip);
-      totalCost += costOfTrip;
+        const costOfTrip = calculateCostOfTrip(trip);
+        totalCost += costOfTrip;
     });
   
     return totalCost;
-  }
+}
 
-  function updateCostBox(userId) {
+function updateCostBox(userId) {
     const totalCost = calculateTotalCostForUser(userId);  
 
     costsTripsSection.innerHTML = `<h3>Total Cost of Trips: $${totalCost}</h3>`;
-  }
+}
   
 function getUserNameById(userId) {
     const user = travelers.find(traveler => traveler.id === userId)
     return user.name
 }
+
+newTripButton.addEventListener("click", () => {
+    showElement(tripForm);
+    hideElement(dashboard) 
+})
+
+document.getElementById("newTripForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const tripDate = document.getElementById("tripDate").value;
+    const tripDuration = parseInt(document.getElementById("tripDuration").value);
+    const numTravelers = parseInt(document.getElementById("travelers").value);
+    const selectedDestinationId = parseInt(document.getElementById("destination").value);
+    
+    const newTrip = {
+        id: trips.length + 1, 
+        userID: userId,
+        destinationID: selectedDestinationId,
+        travelers: numTravelers,
+        date: tripDate,
+        duration: tripDuration,
+        status: "pending",
+        suggestedActivities: []
+    };
+    const estimatedCost = calculateCostOfTrip(newTrip)
+    trips.push(newTrip);
+  
+    alert(`Estimated Cost for the Trip: $${estimatedCost}`);
+    displayTrips(userId, "pending");
+  
+    event.target.reset();
+    hideElement(tripForm)
+    showElement(dashboard)
+});
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const destinationSelect = document.getElementById("destination");
+    destinationSelect.innerHTML = destinations
+    .map((destination) => {
+        return `<option value="${destination.id}">${destination.destination}</option>`;
+    })
+    .join("");
+});
